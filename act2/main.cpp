b6b09e04ed840e043181d3d5ed1a7e268b5760f0
varcoss <varcos@tuta.io>
2024-09-21 15:46:07 -0700

    final changes to code

--- act2/main.cpp
+++ act2/main.cpp
@@ -2,35 +2,48 @@
 #include <vector>
 #include <thread>
 
+//global array
+//std::vector<int> my_array = {1,2,3,4,5,6,7,8,9,10};
+
+
 // Function to increment array elements
-void increment_array_elements(std::vector<int>& array, int start, int end) {
-    for (int i = start; i < end; ++i) {
-        array[i] += 1;
-        std::cout << "Thread: " << pthread_self() << ", Incremented array[" << i << "] = " << array[i] << std::endl; //modify array name as you have in your program. pthread_self() will print out ID of the thread.
-    }
+void increment_array_elements() {
+	std::vector<int> my_array = {1,2,3,4,5,6,7,8,9,10};
+
+    for (int i = 0; i < my_array.size(); ++i) {
+	my_array[i] += 1;
+        std::cout << "Thread: " << pthread_self() << ", Incremented array[" << i << "] = " << my_array[i] << std::endl; //modify array name as you have in your program. pthread_self() will print out ID of the thread.
+    	}
+	std::cout << "Modified arrray with multi-threading: ";
+	for (const auto& element : my_array) {
+		std::cout<<element<< " ";
+		}
+		std::cout<<std::endl;
+    
+    
 }
 
 int main() {
     // Define the array
-    std::vector<int> my_array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    //std::vector<int> my_array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
     // Divide the array into two halves
-    int midpoint = my_array.size() / 2;
+    //int midpoint = my_array.size() / 2;
 
     // Create two threads for processing each half of the array
-    std::thread thread1(increment_array_elements, std::ref(my_array), 0, midpoint);
-    std::thread thread2(increment_array_elements, std::ref(my_array), midpoint, my_array.size());
+    std::thread thread1(increment_array_elements);
+    std::thread thread2(increment_array_elements);
 
     // Start the threads and wait for them to finish
     thread1.join();
     thread2.join();
 
     // Print the modified array
-    std::cout << "Modified array with multi-threading: ";
-    for (const auto& element : my_array) {
-        std::cout << element << " ";
-    }
-    std::cout << std::endl;
+    //std::cout << "Modified array with multi-threading: ";
+    //for (const auto& element : my_array) {
+    //    std::cout << element << " ";
+    //}
+    //std::cout << std::endl;
 
     return 0;
 }
