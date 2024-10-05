98ed058561640947f6819c06dec8483111d47db4
varcoss <varcos@tuta.io>
2024-10-05 11:32:12 -0700

    updating main.cpp

--- hw3/main.cpp
+++ hw3/main.cpp
@@ -5,14 +5,20 @@
 #include <mutex>
 #include <algorithm>
 
-void workerFunction(int id, int start, int end){
+std::vector<int> workerFunction(int id, int start, int end){
 	std::cout << "Thread " << id << " is working. " << start << " " << end <<" \n";
+	
+	// local vector that will store prime numbers
+	std::vector<int> local;
+
+	return local;
 }
 
 int main(){
-	
+	std::vector<int> primeNums;	
 	int limit = 0;
-
+	
+	// getting input from user
 	for(int i = 1; i > 0;i++)
 	{
 		std::cin >> limit;
@@ -21,15 +27,17 @@ int main(){
 		{
 			break;
 		}
-		else
+		else //if input invalid, then loop
 		{
 			std::cout<< "input invalid, try again\n";
 		}
 
 	}
 
+	// getting threads of csystem
 	unsigned int numThreads = std::thread::hardware_concurrency();
 
+	//checking for errors
 	if(numThreads == 0)
 	{
 		numThreads = 2;
@@ -37,34 +45,37 @@ int main(){
 
 	std::cout<< "Creating " << numThreads << " threads.\n";
 
+	//creating vector to store the start and end of nums to be used later
 	std::vector<std::pair<int,int>>range;
 	int start = 1;
+	int end = 0;
 	int remaining = limit % numThreads;
-
+	
+	//loop that determines start and end range of digits 
+	// & stores them in a vector 
 	for (int i = 0; i <numThreads; i++)
 	{
-		int end = start + (limit/numThreads);
+		end = start + (limit/numThreads) -1;
 
 		if (i==numThreads - 1)
 		{
-			start = end + remaining;
+			end = end + remaining;
 		}
 
 		range.push_back({start,end});
 		start = end + 1;
 	}
 
-
-
-
+	// vector for threads
 	std::vector<std::thread> threads;
 
+	// starting threads
 	for (unsigned int i = 0; i <numThreads; ++i)
 	{
-	
-		threads.emplace_back(workerFunction,i,range[i].first, range[i].second);
+		primeNums = threads.emplace_back(workerFunction,i,range[i].first, range[i].second);
 	}
 	
+	//waiting for threads
 	for(auto& t:threads)
 	{
 		t.join();
